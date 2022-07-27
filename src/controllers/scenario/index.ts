799b4a2d1740d58b866b1b9fcaa4311cf411485c
n3op2 <paulius.michelevicius@gmail.com>
2022-07-27 18:27:59 +0100

    DSBD-20: sorting config.

--- src/controllers/scenario/index.ts
+++ src/controllers/scenario/index.ts
@@ -11,13 +11,17 @@ import Logger from '../../utils/Logger'
 
 let log = Logger.child({ controller: '/scenario' });
 
-@Route('run')
+@Route('scenario')
 export class scenario extends Controller {
   @Get('{id}')
   public get(@Path() id: string): Promise<Scenario> {
     log.info(`executing ${id} scenario`);
 
     return new Promise((resolve) => {
+      // run program locally
+      // [ ] - create an array of programs so more scenarios can be added
+      // [ ] - helpeer for running programs
+      // [ ] - 
       exec("ls -la", (error, stdout, stderr) => {
         resolve({
           status: (error || stderr || error) ? 'error': 'success',
