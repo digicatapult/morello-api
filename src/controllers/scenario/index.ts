9dccb4ef499fc4ac57cf753596bac2883fe42a40
n3op2 <paulius.michelevicius@gmail.com>
2022-07-27 18:17:32 +0100

    DSBD-20: clean up.

--- src/controllers/scenario/index.ts
+++ src/controllers/scenario/index.ts
@@ -13,21 +13,15 @@ let log = Logger.child({ controller: '/scenario' });
 
 @Route('run')
 export class scenario extends Controller {
-
   @Get('{id}')
   public get(@Path() id: string): Promise<Scenario> {
     log.info(`executing ${id} scenario`);
 
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       exec("ls -la", (error, stdout, stderr) => {
-        if (error) reject({
-          status: 'error',
-          output: error,
-        })
-
         resolve({
-          status: stderr ? 'error': 'success',
-          output: stderr ? stderr : stdout,
+          status: (error || stderr || error) ? 'error': 'success',
+          output: error ? error.message : stderr ? stderr : stdout,
         });
       })
     })
