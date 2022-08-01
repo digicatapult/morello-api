20c69055cec434f887cc0f810452f900c6af159f
n3op2 <paulius.michelevicius@gmail.com>
2022-08-01 11:15:41 +0100

    DSBD-20: added example folder and updated controller.

--- src/controllers/scenario/index.ts
+++ src/controllers/scenario/index.ts
@@ -4,32 +4,48 @@ import {
   Path,
   Route,
 } from "tsoa";
+import config from 'config';
 import { exec } from 'child_process';
-import { Scenario } from '../../../types'
+// import { Scenario } from '../../../types'
+import Logger from '../../utils/Logger';
 
-import Logger from '../../utils/Logger'
-
-
-let log = Logger.child({ controller: '/scenario' });
+let log = Logger.child({ controller: '/scenario', morello_host: config.get('morello_host') });
 
 @Route('scenario')
 export class scenario extends Controller {
   @Get('{id}')
-  public get(@Path() id: string): Promise<Scenario> {
-    log.info(`executing ${id} scenario`);
-
-    return new Promise((resolve) => {
-      // run program locally
-      // [ ] - create an array of programs so more scenarios can be added
-      // [ ] - helpeer for running programs
-      // [ ] - 
-      exec("ls -la", (error, stdout, stderr) => {
-        resolve({
-          status: (error || stderr || error) ? 'error': 'success',
-          output: error ? error.message : stderr ? stderr : stdout,
+  public async get(@Path() id: string): Promise<any> {
+    log.info(`copying ${id} scenario onto morello host ${config.get('morello_host.address')}`)
+    await new Promise((resolve) => {
+      exec("scp ../../../examples/tmp.bin", (error, stdout, stderr) => {
+        if (error || stderr) {
+          log.error({ error, stderr })
+          throw new Error(`copying ${id} binaries has failed.`)
+        } 
+        log.info('binaries file has been copied')
+        resolve(stdout)
+      });
+    })
+  
+    return ({
+      self: await new Promise((resolve) => {
+        log.info(`executing ${id} scenario on this host`);
+        exec("ls -la", (error, stdout, stderr) => {
+          resolve({
+            status: (error || stderr || error) ? 'error': 'success',
+            output: error ? error.message : stderr ? stderr : stdout,
+          });
         });
+      }),
+      morello: await new Promise((resolve) => {
+        log.info(`executing ${id} scenario on morello host`);
+        exec("ls -la", (error, stdout, stderr) => {
+          resolve({
+            status: (error || stderr || error) ? 'error': 'success',
+            output: error ? error.message : stderr ? stderr : stdout,
+          });
+        })
       })
     })
   }
 }
-
