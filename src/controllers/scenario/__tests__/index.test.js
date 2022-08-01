5589e549900b3c9ca5f407eb13d698c02029b5f0
n3op2 <paulius.michelevicius@gmail.com>
2022-08-01 08:47:40 +0100

    DSBD-20: workflows update.

---
+++ src/controllers/scenario/__tests__/index.test.js
@@ -0,0 +1,6 @@
+describe('/scenario controller', () => {
+  // This is a placeholder and will be updated shortly
+  it('exectures commands and returns output', () => {
+    expect(1).toBe(1)
+  })
+})
