16c74a340acf563c2e413b1e5d9bd6a59e5b4375
n3op2 <paulius.michelevicius@gmail.com>
2022-08-01 16:10:12 +0100

    DSBD-20: updating eslint config.

--- src/controllers/scenario/index.ts
+++ src/controllers/scenario/index.ts
@@ -10,27 +10,29 @@ import { exec } from 'child_process'
 import Logger from '../../utils/Logger'
 
 let log = Logger.child({ controller: '/scenario', morello_host: config.get('morello_host') });
-const { address } = config.get('morello_host')
+
+const { address, username } = config.get('morello_host')
 
 @Route('scenario')
 export class scenario extends Controller {
   @Get('{id}')
   public async get(@Path() id: string): Promise<any> {
+    const cheriBin = `cat ./binaries/${id}-cheri | ssh ${username}@${address} "cat >${id}; chmod +x ${id}; ${id} with args; rm ${id}`
     return ({
       self: await new Promise((resolve) => {
-        log.info(`executing ${id} scenario on this host`);
+        log.info(`executing ${id} example on self`);
 
-        exec('ls', (error, stdout, stderr) => resolve({
-          status: (error || stderr) ? 'error': 'success',
-          output: error ? error : stderr ? stderr : stdout,
+        exec(`./binaries/${id}-aarch64`, (err, stdout, stderr) => resolve({
+          status: (err || stderr) ? 'error': 'success',
+          output: err ? err : stderr ? stderr : stdout,
         }))
       }),
       morello: await new Promise((resolve) => {
-        log.info(`executing ${id} scenario on morello host`);
+        log.info(`executing ${id} exampole on the morello host`);
 
-        exec(`ssh ${address} ls`, (error, stdout, stderr) => resolve({
-          status: (error || stderr) ? 'error': 'success',
-          output: error ? error : stderr ? stderr : stdout,
+        exec(cheriBin, (err, stdout, stderr) => resolve({
+          status: (err || stderr) ? 'error': 'success',
+          output: err ? err : stderr ? stderr : stdout,
         }));
       }),
     })
