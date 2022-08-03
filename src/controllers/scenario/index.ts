6abd0c657131f3bf351668ebca7e7e6ad1bd379a
n3op2 <paulius.michelevicius@gmail.com>
2022-08-03 16:48:26 +0100

    DSBD-31: change way of clearing tmp binaries as before it was returning an exception.

--- src/controllers/scenario/index.ts
+++ src/controllers/scenario/index.ts
@@ -22,17 +22,22 @@ export class scenario extends Controller implements IScenario {
     this.log = Logger.child({ controller: '/scenario', ...config.get('morello') })
   }
 
+
   execute(bin: string): Promise<HostResponse> {
     const scp = `scp -P ${this.port} bin/${bin} ${this.address}:/tmp`
-    const ssh = `ssh -p ${this.port} ${this.address} -tt 'chmod +x /tmp/${bin}; /tmp/${bin}; rm /tmp/${bin}' 2>&1`
+    const ssh = `ssh -p ${this.port} ${this.address} -tt 'chmod +x /tmp/${bin}; /tmp/${bin} with args' 2>&1`
+    const rm = `ssh -p ${this.port} ${this.address} rm -v /tmp/${bin} &> /dev/null`
     this.log.debug({ msg: `executing ${bin} on ${this.address} host`, scp, ssh })
 
     return new Promise((resolve) => {
-      exec(`${scp}; ${ssh}`, (err, stdout) => resolve({
-        status: err ? 'error': 'success',
-        output: stdout,
-        ...err ? { exception: err } : {}
-      }))
+      exec(`${scp}; ${ssh}`, (err, stdout) => {
+        exec(rm)
+        return resolve({
+          status: err ? 'error': 'success',
+          output: stdout,
+          ...err ? { exception: err } : {}
+        })
+      })
     })
   }
   
