46a2cd9ce3f7fb5081f2a801b57bea162f18ef9f
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-08-30 11:38:41 +0100

    Add in other examples to openapi type

--- types/models/scenario.ts
+++ types/models/scenario.ts
@@ -2,10 +2,16 @@ import { ExecException } from 'child_process'
 import Logger from '../../src/utils/Logger'
 
 export type Executables =
+  'out-of-bounds-access-aarch64' |
+  'out-of-bounds-access-cheri' |
   'out-of-bounds-read-aarch64' |
   'out-of-bounds-read-cheri' |
+  'out-of-bounds-readV2-aarch64' |
+  'out-of-bounds-readV2-cheri' |
   'out-of-bounds-write-aarch64' |
-  'out-of-bounds-write-cheri'
+  'out-of-bounds-write-cheri' |
+  'use-after-free-aarch64' |
+  'use-after-free-cheri'
 
 export type HostResponse = {
   status: 'success' | 'error' | 'exception',
