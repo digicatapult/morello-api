34c3ac4a7bb0ec687edcac878973ccdb378f7bf6
Matthew Dean <matthew.dean@digicatapult.org.uk>
2022-08-30 10:33:06 +0100

    Linting fix

--- src/utils/__tests__/params.test.js
+++ src/utils/__tests__/params.test.js
@@ -7,40 +7,44 @@ describe('getRandomProcessName', () => {
   })
 
   it('should always return a different string', () => {
-    const exampleSet = new Set(Array(1000).fill(null).map(_ => getRandomProcessName('test')))
+    const exampleSet = new Set(
+      Array(1000)
+        .fill(null)
+        .map(() => getRandomProcessName('test'))
+    )
     expect(exampleSet.size).to.equal(1000)
   })
 })
 
 describe('getValidHeredocEOF', () => {
-  it("should by default returns EOF", () => {
-    expect(getValidHeredocEOF("test", [])).to.equal("EOF")
+  it('should by default returns EOF', () => {
+    expect(getValidHeredocEOF('test', [])).to.equal('EOF')
   })
 
-  it("should append _ if bin contains EOF", () => {
-    expect(getValidHeredocEOF("test_EOF", [])).to.equal("EOF_")
+  it('should append _ if bin contains EOF', () => {
+    expect(getValidHeredocEOF('test_EOF', [])).to.equal('EOF_')
   })
 
-  it("should append _ if parameter contains EOF", () => {
-    expect(getValidHeredocEOF("test", ["param_EOF"])).to.equal("EOF_")
+  it('should append _ if parameter contains EOF', () => {
+    expect(getValidHeredocEOF('test', ['param_EOF'])).to.equal('EOF_')
   })
 
-  it("should append __ if bin contains EOF_", () => {
-    expect(getValidHeredocEOF("test_EOF_", [])).to.equal("EOF__")
+  it('should append __ if bin contains EOF_', () => {
+    expect(getValidHeredocEOF('test_EOF_', [])).to.equal('EOF__')
   })
 
-  it("should append __ if parameter contains EOF_", () => {
-    expect(getValidHeredocEOF("test_EOF", ["param_EOF_"])).to.equal("EOF__")
+  it('should append __ if parameter contains EOF_', () => {
+    expect(getValidHeredocEOF('test_EOF', ['param_EOF_'])).to.equal('EOF__')
   })
 
-  it (`should append _____ if bin contains EOF____`, () => {
-    expect(getValidHeredocEOF("test_EOF____test", [])).to.equal("EOF_____")
+  it(`should append _____ if bin contains EOF____`, () => {
+    expect(getValidHeredocEOF('test_EOF____test', [])).to.equal('EOF_____')
   })
 })
 
 describe('escapeParams', () => {
   it('returns input wrapped in quotes', () => {
-    expect(escapeParam("arg1")).to.equal(`'arg1'`)
+    expect(escapeParam('arg1')).to.equal(`'arg1'`)
   })
 
   it('replaces single quotes with an escaped quote', () => {
