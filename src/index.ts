b988323a2875319f39ff10f22749f259cbc2be73
Jonathan Gray <jonmattgray@gmail.com>
2022-09-02 14:13:20 +0100

    revert linting

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,73 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Path,
+  Route,
+  Response,
+} from 'tsoa'
 import config from 'config'
-import { urlencoded, json } from 'body-parser'
-import * as swaggerUI from 'swagger-ui-express'
-import express, { Express } from 'express'
-import cors from 'cors'
+import { exec } from 'child_process'
+import { IScenario, HostResponse, Executables } from '../../../types'
+import Logger from '../../utils/Logger'
+import * as paramUtil from '../../utils/params'
+import * as execUtil from '../../utils/executables'
+import { ValidateErrorJSON, ScenarioNotFoundJSON, ScenarioNotFoundError } from '../../utils/errors'
 
-import { RegisterRoutes } from './routes'
-import * as swaggerJson from './swagger.json'
+@Route('scenario')
+export class scenario extends Controller implements IScenario {
+  address: string
+  port: number
+  log: typeof Logger
 
-import { errorHandler } from './utils/errors'
-import  { validateExecutables } from './utils/executables'
-import logger from './utils/Logger'
+  constructor() {
+    super()
+    this.address = `${config.get('morello.username')}@${config.get('morello.host')}`
+    this.port = config.get('morello.port')
+    this.log = Logger.child({ controller: '/scenario', ...config.get('morello') })
+  }
 
-const start = async () => {
-  await validateExecutables()
+  async execute(bin: Executables, params: string[] = []): Promise<HostResponse> {
+    if (!await execUtil.checkExecutable(bin)) {
+      throw new ScenarioNotFoundError(bin)
+    }
 
-  const app: Express = express()
-  const port: Number = config.get('app.port')
+    params = params.map(p => paramUtil.escapeParam(p))
+    const destBin = paramUtil.getRandomProcessName(bin)
+    const eof = paramUtil.getValidHeredocEOF(bin, params)
 
-  app.use(urlencoded({ extended: true }))
-  app.use(json())
-  app.use(cors())
-  RegisterRoutes(app)
-  app.use(['/openapi', '/docs', '/swagger'], swaggerUI.serve, swaggerUI.setup(swaggerJson))
-  app.use(errorHandler)
+    const scp = `scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -P ${this.port} bin/${bin} ${this.address}:/tmp/${destBin}`
+    const ssh =
+`ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -p ${this.port} ${this.address} -t << '${eof}'
+chmod +x /tmp/${bin};
+/tmp/${destBin} ${params.join(' ')} 2>&1;
+exit;
+${eof}`
 
-  app.listen(port, () => {
-    logger.info('Server is running')
-  })
+    const rm = `ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -p ${this.port} ${this.address} rm -v /tmp/${destBin} &> /dev/null`
+    this.log.debug({ msg: `executing ${bin} on ${this.address} host`, scp, ssh })
+
+    return new Promise((resolve) => {
+      exec(`${scp}; ${ssh}`, (err, stdout) => {
+        exec(rm) // fire and forget, remove binary file
+        return resolve(err ? {
+          status: 'error',
+          output: stdout,
+          exception: err
+        } : {
+          status: 'success',
+          output: stdout,
+        })
+      })
+    })
+  }
+
+  @Response<ValidateErrorJSON>(422, "Validation Failed")
+  @Response<ScenarioNotFoundJSON>(501, "Error executing scenario (not found)")
+  @Get('{executable}')
+  public async get(@Path() executable: Executables , @Query() params?: string[]): Promise<HostResponse> {
+    this.log.debug(`attempting to execute ${executable} scenario with [${params}] arguments`)
+
+    return this.execute(executable, params)
+  }
 }
-start()
-
-
