e545d55bf5d559596bf1f3284ad25420c9fa7069
Mark Addis <nitro_charlie@live.com>
2022-09-05 15:07:31 +0100

    Tests should work

--- integration/helper/routeHelper.ts
+++ integration/helper/routeHelper.ts
@@ -1,47 +1,47 @@
-import * as request from 'supertest'
+import request from 'supertest'
 
-export async function getOutOfBoundsReadAarch64(app, password, firstOffset, secondOffset) {
+export async function getOutOfBoundsReadAarch64(app: any, password: any, firstOffset: any, secondOffset: any) {
   return request(app)
     .get(`/scenario/out-of-bounds-readV2-aarch64?params=${password}&params=${firstOffset}&params=${secondOffset}`)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json')
     .send()
-    .then((response) => {
+    .then((response: any) => {
       return response
     })
-    .catch((err) => {
+    .catch((err: any) => {
       // eslint-disable-next-line no-console
       console.error(`Out of bounds read (aaarch64) ${err}`)
       return err
     })
 }
 
-export async function getOutOfBoundsReadCheri(app, password, firstOffset, secondOffset) {
+export async function getOutOfBoundsReadCheri(app: any, password: any, firstOffset: any, secondOffset: any) {
   return request(app)
     .get(`/scenario/out-of-bounds-readV2-cheri?params=${password}&params=${firstOffset}&params=${secondOffset}`)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json')
     .send()
-    .then((response) => {
+    .then((response: any) => {
       return response
     })
-    .catch((err) => {
+    .catch((err: any) => {
       // eslint-disable-next-line no-console
       console.error(`Out of bounds read (aaarch64) ${err}`)
       return err
     })
 }
 
-export async function getInvalidExecutable(app, password) {
+export async function getInvalidExecutable(app: any, password: any) {
   return request(app)
     .get(`/scenario/out-of-bounds-readV5-aarch64?params=${password}`)
     .set('Accept', 'application/json')
     .set('Content-Type', 'application/json')
     .send()
-    .then((response) => {
+    .then((response: any) => {
       return response
     })
-    .catch((err) => {
+    .catch((err: any) => {
       // eslint-disable-next-line no-console
       console.error(`Out of bounds read (aaarch64) ${err}`)
       return err
