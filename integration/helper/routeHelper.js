579742d48dfc0be0cb9041cb6706e37b21355760
Mark Addis <nitro_charlie@live.com>
2022-09-05 10:15:44 +0100

    First test

--- integration/helper/routeHelper.js
+++ integration/helper/routeHelper.js
@@ -16,6 +16,23 @@ async function getOutOfBoundsReadAarch64(app, password, firstOffset, secondOffse
     })
 }
 
+async function getOutOfBoundsReadCheri(app, password, firstOffset, secondOffset) {
+  return request(app)
+    .get(`/scenario/out-of-bounds-readV2-cheri?params=${password}&params=${firstOffset}&params=${secondOffset}`)
+    .set('Accept', 'application/json')
+    .set('Content-Type', 'application/json')
+    .send()
+    .then((response) => {
+      return response
+    })
+    .catch((err) => {
+      // eslint-disable-next-line no-console
+      console.error(`Out of bounds read (aaarch64) ${err}`)
+      return err
+    })
+}
+
 module.exports = {
   getOutOfBoundsReadAarch64,
+  getOutOfBoundsReadCheri,
 }
--- integration/out-of-bounds-read.test.js
+++ integration/out-of-bounds-read.test.js
@@ -1,7 +1,7 @@
 const { describe, before, test } = require('mocha')
 const { expect } = require('chai')
 const { CreateHttpServer } = require('../src/index')
-const { getOutOfBoundsReadAarch64 } = require('./helper/routeHelper')
+const { getOutOfBoundsReadAarch64, getOutOfBoundsReadCheri } = require('./helper/routeHelper')
 
 describe('Tests aarch64 version', () => {
   let app
@@ -10,10 +10,53 @@ describe('Tests aarch64 version', () => {
     app = await CreateHttpServer()
   })
 
-  test('Happy Path', async function () {
+  test('Happy Path', async () => {
     const response = await getOutOfBoundsReadAarch64(app, 'pass', -32, -28)
 
     expect(response.status).to.equal(200)
-    expect(response.body.output).to.contain('pass')
+    expect(response.body.status).to.contain('success')
+  })
+
+  test('Missing Parameters', async () => {
+    const response = await getOutOfBoundsReadAarch64(app, 'pass_1', -32)
+
+    expect(response.status).to.equal(200) // Should be 400?
+    expect(response.body.status).to.contain('success') //should be 'error'
+  })
+
+  test('Bad Parameters', async () => {
+    const response = await getOutOfBoundsReadAarch64(app, 'badpass', NaN, 'ttttttt')
+
+    expect(response.status).to.equal(200) // Should be 400?
+    expect(response.body.status).to.contain('success') // Should be an error?
+  })
+})
+
+describe('Tests Cheri version', () => {
+  let app
+
+  before(async () => {
+    app = await CreateHttpServer()
+  })
+
+  test('Happy Path', async () => {
+    const response = await getOutOfBoundsReadCheri(app, 'cheripass', -32, -21)
+
+    expect(response.status).to.equal(200)
+    expect(response.body.status).to.contain('error')
+  })
+
+  test('Missing Parameter', async () => {
+    const response = await getOutOfBoundsReadCheri(app, 'cheripass', -32)
+
+    expect(response.status).to.equal(200) //Should be 400?
+    expect(response.body.status).to.contain('error')
+  })
+
+  test('Bad Parameter', async () => {
+    const response = await getOutOfBoundsReadCheri(app, 'cheripass', 'cube', 'tarmac')
+
+    expect(response.status).to.equal(200) //Should be 400?
+    expect(response.body.status).to.contain('success') //Should be an error?
   })
 })
